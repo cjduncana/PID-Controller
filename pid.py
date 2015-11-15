3a08ac56671fd83a82649cf5ae1841347943b55a
Christopher Duncan <cj.duncan.a@gmail.com>
2015-11-14 19:33:23 -0500

    Create the PID controller
    Create a PID controller module to regulate a variable around a set
    point. It has default values for the time interval and the PID
    coefficients. It also creates an empty list to function as the PID
    model.

---
+++ pid.py
@@ -0,0 +1,25 @@
+
+from decimal import Decimal
+
+class PID:
+
+    def __init__(self, setPoint, timeInterval = Decimal("10"), kp = Decimal("1"), ki = Decimal("0.0005"), kd = Decimal("10")):
+        self.model = []
+        self.previousError = Decimal("0")
+        self.sumOfPastErrors = Decimal("0")
+        self.setPoint = setPoint
+        self.timeInterval = timeInterval
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+
+    def update(self):
+        if self.model:
+            error = self.setPoint - self.model.pop()
+            self.sumOfPastErrors = self.sumOfPastErrors + (error * self.timeInterval)
+            changeInError = (error - self.previousError) / self.timeInterval
+            controlVariable = self.kp * error + self.ki * self.sumOfPastErrors + self.kd * changeInError
+            self.previousError = error
+            return controlVariable
+
+    
