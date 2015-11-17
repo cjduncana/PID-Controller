52e2fd6d25a981f2db1cdf4836de15de83482dcc
Christopher Duncan <cj.duncan.a@gmail.com>
2015-11-17 14:52:06 -0500

    Change import statements
    According to Python Guide article on structure, it is best to be
    explicit about which module a class is coming from when using that
    module.

--- pid.py
+++ pid.py
@@ -1,28 +1,28 @@
 
-from decimal import Decimal
-from time import time
+import decimal
+import time
 
 class PID:
 
     def __init__(self, setPoint,
-                 kp = Decimal("1"),
-                 ki = Decimal("0.0005"),
-                 kd = Decimal("10")):
+                 kp = decimal.Decimal("1"),
+                 ki = decimal.Decimal("0.0005"),
+                 kd = decimal.Decimal("10")):
         self.setPoint = setPoint
         self.model = []
         self.model.append(setPoint)
 
-        self.previousInput = Decimal("0")
-        self.integral = Decimal("0")
+        self.previousInput = decimal.Decimal("0")
+        self.integral = decimal.Decimal("0")
 
-        self.previousTime = Decimal(time())
+        self.previousTime = decimal.Decimal(time.time())
 
         self.kp = kp
         self.ki = ki
         self.kd = kd
 
     def update(self):
-        now = Decimal(time())
+        now = decimal.Decimal(time.time())
         changeInTime = now - self.previousTime
 
         processVariable = self.model.pop()
